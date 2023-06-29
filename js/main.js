655e4c3fde2b1948f5d71bb36e0a1ddb789a9be3
namo <namo02268@gmail.com>
2023-06-30 01:29:16 +0900

    [Update] deltaXY

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ function main() {
     return;
   }
   document.body.appendChild(canvas);
-  document.querySelector(".version").textContent = `Version: ${2.3.toFixed(1)}`;
+  document.querySelector(".version").textContent = `Version: ${2.4.toFixed(1)}`;
 
   //-------------------Init Scene-------------------//
   const scene = new Scene();
